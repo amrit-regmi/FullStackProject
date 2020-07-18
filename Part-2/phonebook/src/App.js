c98e74884703e3c628a5615fb8125bc0ea70e7fd
amrit <rit.regmi@gmail.com>
2020-07-19 01:06:16 +0300

    Part 2-11 complete

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './Components/Filter'
 import NewPersonForm from './Components/NewPersonForm'
 import Persons from './Components/Persons'
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [filterText,setFilterText] = useState('')
 
+  useEffect(()=>{
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data) 
+    })
+  },[])
+
   const filtered = persons.filter(person=>person.name.toLowerCase().includes(filterText))
 
+  
   const addName = (event) => {
     event.preventDefault()
     if (persons.filter(person => person.name === newName).length > 0){
